83e6ff9313f7195ddc8cf6527e439fa9ddf53994
Rogerio-17 <rogeriojmf10@gmail.com>
2024-04-04 15:53:11 -0300

    Finalizando a segunda aula

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import nlwUnitIcon from "../assets/nlw-unite-icon.svg"
+import { NavLink } from "./nav-link"
 
 export function Header() {
     return (
@@ -6,8 +7,8 @@ export function Header() {
             <img src={nlwUnitIcon} />
 
             <nav className="flex items-center gap-5 py-2"> 
-                <a href="" className="font-medim text-sm text-zinc-300">Eventos</a>
-                <a href="" className="font-medim text-sm">Participantes</a>
+                <NavLink href="/eventos">Eventos</NavLink>
+                <NavLink href="/participantes">Participantes</NavLink>
             </nav>
         </div>
     )
